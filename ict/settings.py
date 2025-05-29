2ddf2518a971fbdbea19cd5018237474a8d1e12b
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-05-29 02:02:23 -0400

    remove django-exes

--- ict/settings.py
+++ ict/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'axes.middleware.AxesMiddleware',
+    # 'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'ict.urls'
